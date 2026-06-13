b68d28237f5fdc0cef36f9a45cfa0938e68d1f77
VishaL18-2 <kkenterprisesdev@gmail.com>
2026-06-13 13:55:10 +0530

    feat: add and map tryon assets for all products

--- src/data/products.js
+++ src/data/products.js
@@ -6,7 +6,9 @@ export const products = [
     "price": "₹359",
     "image": "https://images.meesho.com/images/catalogs/300178929/cover/1/2/960bdc022f34a875747bda0d38c0fadd3d0fa4243bd54875e795f303337407ae505b5750d33f1d74350eed7c24343356aff36bdd918fcde063f584f9e5bb3e2d_512.jpg",
     "meeshoLink": "https://www.meesho.com/elegant-teardrop-red-stone-pendant-necklace-set-stylish-silver-finish-crystal-jewelry-for-women-girls-perfect-for-party-formal-wear/p/boxwfv",
-    "tryOnImage": "/assets/tryon_necklace_300178929.png"
+    "tryOnImage": "/assets/tryon_necklace_300178929.png",
+    "tryOnImageLeft": "/assets/tryon_earring_left_300178929.png",
+    "tryOnImageRight": "/assets/tryon_earring_right_300178929.png"
   },
   {
     "id": 403919628,
@@ -33,7 +35,9 @@ export const products = [
     "description": "Classy Earrings & Studs",
     "price": "₹146",
     "image": "https://images.meesho.com/images/catalogs/299898536/cover/1/2/9ba86e127797ee8e1be475f84eb56f21114d329e09b3b7039365a8c19be1e3ab7df234f59c42404ab71a2f019af740b303f9a849edd770a067588cce6671e769_512.jpg",
-    "meeshoLink": "https://www.meesho.com/unique-oxidized-silver-garba-dancer-motif-jhumkas-traditional-artistic-hanging-earrings-for-women-girls-stylish-lightweight-ethnic-jewelry/p/boqr0m"
+    "meeshoLink": "https://www.meesho.com/unique-oxidized-silver-garba-dancer-motif-jhumkas-traditional-artistic-hanging-earrings-for-women-girls-stylish-lightweight-ethnic-jewelry/p/boqr0m",
+    "tryOnImageLeft": "/assets/tryon_earring_left_299898536.png",
+    "tryOnImageRight": "/assets/tryon_earring_right_299898536.png"
   },
   {
     "id": 300227382,
@@ -41,7 +45,10 @@ export const products = [
     "description": "Twinkling Charming Women Necklaces & Chains",
     "price": "₹364",
     "image": "https://images.meesho.com/images/catalogs/300227382/cover/1/2/f024996048902adbe4df0a6570d46dbd23dccd76c640cabf534f7b1e018f03e2266bb843de8ef6caa1c6305708e6d12249a2c7654374a064de2f5ef57e9cff0d_512.jpg",
-    "meeshoLink": "https://www.meesho.com/elegant-floral-pendant-necklace-set-with-matching-earrings-dainty-silver-finish-jewelry-for-women-girls-stylish-daily-wear-ethnic-accessory/p/boz2p6"
+    "meeshoLink": "https://www.meesho.com/elegant-floral-pendant-necklace-set-with-matching-earrings-dainty-silver-finish-jewelry-for-women-girls-stylish-daily-wear-ethnic-accessory/p/boz2p6",
+    "tryOnImage": "/assets/tryon_necklace_300227382.png",
+    "tryOnImageLeft": "/assets/tryon_earring_left_300227382.png",
+    "tryOnImageRight": "/assets/tryon_earring_right_300227382.png"
   },
   {
     "id": 403918765,
@@ -49,7 +56,10 @@ export const products = [
     "description": "Twinkling Colorful Jewellery Sets",
     "price": "₹548",
     "image": "https://images.meesho.com/images/catalogs/403918765/cover/1/2/ffaee101dac76b8881687aa3c535056cd649495d6a0d1485313039c3593d37f5f3c8811d064c1d4c61b411a2211d916d7cb9057c5b0e900060691d9a9b26a230_512.jpg",
-    "meeshoLink": "https://www.meesho.com/traditional-gold-plated-temple-necklace-set-with-meenakari-work-ethnic-kundan-choker-with-matching-jhumkas-elegant-jewelry-for-wedding-festive-look/p/ds5140"
+    "meeshoLink": "https://www.meesho.com/traditional-gold-plated-temple-necklace-set-with-meenakari-work-ethnic-kundan-choker-with-matching-jhumkas-elegant-jewelry-for-wedding-festive-look/p/ds5140",
+    "tryOnImage": "/assets/tryon_necklace_403918765.png",
+    "tryOnImageLeft": "/assets/tryon_earring_left_403918765.png",
+    "tryOnImageRight": "/assets/tryon_earring_right_403918765.png"
   },
   {
     "id": 299834782,
@@ -57,7 +67,9 @@ export const products = [
     "description": "Classy Earrings & Studs",
     "price": "₹159",
     "image": "https://images.meesho.com/images/catalogs/299834782/cover/1/2/a15a27dec900e27e92a7548d395b9f20e52b7db62d709ee2f0a695332a303acaa7114146a91b14b70cb9f29c5a5d738c5e0c3cbe49b1ac9e7357900fe68f35da_512.jpg",
-    "meeshoLink": "https://www.meesho.com/exquisite-oxidized-silver-peacock-feather-mor-pankh-jhumkas-traditional-bohemian-style-earrings-with-pearl-beads-festive-ethnic-jewelry-for-women/p/bop87t"
+    "meeshoLink": "https://www.meesho.com/exquisite-oxidized-silver-peacock-feather-mor-pankh-jhumkas-traditional-bohemian-style-earrings-with-pearl-beads-festive-ethnic-jewelry-for-women/p/bop87t",
+    "tryOnImageLeft": "/assets/tryon_earring_left_299834782.png",
+    "tryOnImageRight": "/assets/tryon_earring_right_299834782.png"
   },
   {
     "id": 299907685,
@@ -65,7 +77,9 @@ export const products = [
     "description": "Latest Earrings & Studs",
     "price": "₹135",
     "image": "https://images.meesho.com/images/catalogs/299907685/cover/1/2/93f5c3dee6e67d36c4f435dba77f6072d9af0bc7b1445f8dae1b15e278d8b602035948bcd1628bb73acf61485facc6db1dbb5262619ddcf81668ab63df3f50d7_512.jpg",
-    "meeshoLink": "https://www.meesho.com/elegant-dual-toned-oxidized-jhumka-earrings-with-floral-detail-stylish-traditional-brass-silver-finish-earrings-for-women-girls-lightweight-festive-ethnic-jewelry/p/boqyob"
+    "meeshoLink": "https://www.meesho.com/elegant-dual-toned-oxidized-jhumka-earrings-with-floral-detail-stylish-traditional-brass-silver-finish-earrings-for-women-girls-lightweight-festive-ethnic-jewelry/p/boqyob",
+    "tryOnImageLeft": "/assets/tryon_earring_left_299907685.png",
+    "tryOnImageRight": "/assets/tryon_earring_right_299907685.png"
   },
   {
     "id": 299910228,
@@ -73,7 +87,9 @@ export const products = [
     "description": "New Earrings & Studs",
     "price": "₹135",
     "image": "https://images.meesho.com/images/catalogs/299910228/cover/1/2/7a7ad215825881f123f7c6410f5338a484b66330c21f7f21745a55bd468baf35d76ce215943e1f4009a7cf7ea1de3db263526859dd95c2551e8a53e4fa0d0521_512.jpg",
-    "meeshoLink": "https://www.meesho.com/elegant-3-layer-gold-plated-long-jhumka-earrings-with-pearl-accents-stylish-traditional-latkan-earrings-for-women-girls-lightweight-festive-ethnic-jewelry/p/bor12e"
+    "meeshoLink": "https://www.meesho.com/elegant-3-layer-gold-plated-long-jhumka-earrings-with-pearl-accents-stylish-traditional-latkan-earrings-for-women-girls-lightweight-festive-ethnic-jewelry/p/bor12e",
+    "tryOnImageLeft": "/assets/tryon_earring_left_299910228.png",
+    "tryOnImageRight": "/assets/tryon_earring_right_299910228.png"
   },
   {
     "id": 373288347,
@@ -89,7 +105,10 @@ export const products = [
     "description": "Allure Chunky Women Necklaces & Chains",
     "price": "₹230",
     "image": "https://images.meesho.com/images/catalogs/373544093/cover/1/2/12f58ab7f839f61cce89c9e81e988339bf30b94fb41ee798bbb2c206391580d767711fb372420e20f66dd08a113b01db0c274a600bbe93d3af725fcec660b9a6_512.jpg",
-    "meeshoLink": "https://www.meesho.com/elegant-7-layer-multi-strand-necklace-set-with-matching-earrings-traditional-pearl-bead-layered-chain-for-women-festive-ethnic-jewelry/p/d8aun4"
+    "meeshoLink": "https://www.meesho.com/elegant-7-layer-multi-strand-necklace-set-with-matching-earrings-traditional-pearl-bead-layered-chain-for-women-festive-ethnic-jewelry/p/d8aun4",
+    "tryOnImage": "/assets/tryon_necklace_373544093.png",
+    "tryOnImageLeft": "/assets/tryon_earring_left_373544093.png",
+    "tryOnImageRight": "/assets/tryon_earring_right_373544093.png"
   },
   {
     "id": 299962529,
@@ -97,7 +116,8 @@ export const products = [
     "description": "Shimmering Charming Women Necklaces & Chains",
     "price": "₹226",
     "image": "https://images.meesho.com/images/catalogs/299962529/cover/1/2/2466cdca4f465d1fac87e03bf09f3da1bbff1953a1036e0efb08a09cc98113950a937002c4cfaccaefc4dfbd4e9a5bb928154b04cdb442445b9d956c4e4f79cf_512.jpg",
-    "meeshoLink": "https://www.meesho.com/stylish-chain-with-pendant-for-girls-gold-silver-pendant-chain-jewellerychain-with-pendant-for-women-gold-plated-silver-chain-pendant-with-earrings-set-stylishchain-with-pendant-made-of-flower-resin-stylish-handmade-pendant-chain-for-women/p/bosby0"
+    "meeshoLink": "https://www.meesho.com/stylish-chain-with-pendant-for-girls-gold-silver-pendant-chain-jewellerychain-with-pendant-for-women-gold-plated-silver-chain-pendant-with-earrings-set-stylishchain-with-pendant-made-of-flower-resin-stylish-handmade-pendant-chain-for-women/p/bosby0",
+    "tryOnImage": "/assets/tryon_necklace_299962529.png"
   },
   {
     "id": 373629532,
@@ -105,7 +125,10 @@ export const products = [
     "description": "Twinkling Chunky Women Necklaces & Chains",
     "price": "₹321",
     "image": "https://images.meesho.com/images/catalogs/373629532/cover/1/2/529d204c4e57647ef11b4577e28e5937e1a4b4121e657d0fb25637af1f86cb420973df0feb2860b11809a4043f8ed2099956eeb0b5c7449a37d91becbf099f70_512.jpg",
-    "meeshoLink": "https://www.meesho.com/elegant-gold-plated-mangalsutra-set-with-matching-earrings-traditional-daily-wear-black-bead-necklace-for-women-stylish-ethnic-jewelry-set/p/d8cs7y"
+    "meeshoLink": "https://www.meesho.com/elegant-gold-plated-mangalsutra-set-with-matching-earrings-traditional-daily-wear-black-bead-necklace-for-women-stylish-ethnic-jewelry-set/p/d8cs7y",
+    "tryOnImage": "/assets/tryon_necklace_373629532.png",
+    "tryOnImageLeft": "/assets/tryon_earring_left_373629532.png",
+    "tryOnImageRight": "/assets/tryon_earring_right_373629532.png"
   },
   {
     "id": 299826700,
@@ -113,7 +136,9 @@ export const products = [
     "description": "Casual Earrings & Studs",
     "price": "₹149",
     "image": "https://images.meesho.com/images/catalogs/299826700/cover/1/2/77b5693f60f60b31088c6898921cfb89450cbdd01ecb1bd2a3952bf3e2d1db7584cd90264456986f3940ee9b8630138a71062613f3b225cdff55c7162e47ff33_512.jpg",
-    "meeshoLink": "https://www.meesho.com/trendy-oxidized-silver-hoop-style-jhumkas-with-colorful-beads-stylish-traditional-dangler-earrings-for-women-girls-festive-ethnic-jewelry/p/bop0pj"
+    "meeshoLink": "https://www.meesho.com/trendy-oxidized-silver-hoop-style-jhumkas-with-colorful-beads-stylish-traditional-dangler-earrings-for-women-girls-festive-ethnic-jewelry/p/bop0pj",
+    "tryOnImageLeft": "/assets/tryon_earring_left_299826700.png",
+    "tryOnImageRight": "/assets/tryon_earring_right_299826700.png"
   },
   {
     "id": 300128425,
@@ -121,7 +146,9 @@ export const products = [
     "description": "Fancy Earrings & Studs",
     "price": "₹145",
     "image": "https://images.meesho.com/images/catalogs/300128425/cover/1/2/0d60ef45d7bb5564ea02642f83f760ba640b1abb969209d748c897a888368b160335a5ddc072636bfc98166012b2a573a7fbcc77fd18d2e4d02966815d4da1da_512.jpg",
-    "meeshoLink": "https://www.meesho.com/elegant-gold-plated-3-layer-dangler-earrings-with-colorful-beads-stylish-latkan-jhumkas-for-women-girls-lightweight-ethnic-festive-jewelry/p/bowlw7"
+    "meeshoLink": "https://www.meesho.com/elegant-gold-plated-3-layer-dangler-earrings-with-colorful-beads-stylish-latkan-jhumkas-for-women-girls-lightweight-ethnic-festive-jewelry/p/bowlw7",
+    "tryOnImageLeft": "/assets/tryon_earring_left_300128425.png",
+    "tryOnImageRight": "/assets/tryon_earring_right_300128425.png"
   },
   {
     "id": 299906078,
@@ -129,7 +156,9 @@ export const products = [
     "description": "Fashionable Earrings & Studs",
     "price": "₹153",
     "image": "https://images.meesho.com/images/catalogs/299906078/cover/1/2/defc23f8c19adde133d1dc0c4167481123f19d03872625af019c18de11ccb51526de6f94a310ce590e78aabebcc047f3ff915eb273cd8e7894bba4e8eaf06a65_512.jpg",
-    "meeshoLink": "https://www.meesho.com/unique-oxidized-silver-deer-motif-jhumkas-traditional-artistic-hanging-earrings-for-women-girls-stylish-lightweight-ethnic-jewelry/p/boqx8e"
+    "meeshoLink": "https://www.meesho.com/unique-oxidized-silver-deer-motif-jhumkas-traditional-artistic-hanging-earrings-for-women-girls-stylish-lightweight-ethnic-jewelry/p/boqx8e",
+    "tryOnImageLeft": "/assets/tryon_earring_left_299906078.png",
+    "tryOnImageRight": "/assets/tryon_earring_right_299906078.png"
   },
   {
     "id": 358961612,
@@ -145,7 +174,9 @@ export const products = [
     "description": "Trendy Earrings & Studs",
     "price": "₹130",
     "image": "https://images.meesho.com/images/catalogs/375886438/cover/1/2/5a7b5d59a929cd8698b6aad1fceb1bca143ec3505b13ffe1e9db9009371bdfa491b046e2788b073308119b97350978cbe7dc0abdf009f8026ec06af8ccd655c4_512.jpg",
-    "meeshoLink": "https://www.meesho.com/double-layer-oxidized-silver-jhumkas-with-colorful-beads-bohemian-style-german-silver-earrings-for-women-girls-trendy-traditional-festive-jewelry/p/d9w1kf"
+    "meeshoLink": "https://www.meesho.com/double-layer-oxidized-silver-jhumkas-with-colorful-beads-bohemian-style-german-silver-earrings-for-women-girls-trendy-traditional-festive-jewelry/p/d9w1kf",
+    "tryOnImageLeft": "/assets/tryon_earring_left_375886438.png",
+    "tryOnImageRight": "/assets/tryon_earring_right_375886438.png"
   },
   {
     "id": 299971351,
@@ -153,7 +184,10 @@ export const products = [
     "description": "Elite Chunky Women Necklaces & Chains",
     "price": "₹362",
     "image": "https://images.meesho.com/images/catalogs/299971351/cover/1/2/4d86c847c8e05126364a7d740d169d7d42d8c7607984c1c4adced6d5cc603fe82b1438446229ba70a8419e4ca5bdcdcbde3b971911174dd824c004391ab50804_512.jpg",
-    "meeshoLink": "https://www.meesho.com/elegant-teardrop-emerald-green-pendant-necklace-set-stylish-silver-finish-crystal-jewelry-for-women-girls-perfect-for-party-formal-wear/p/bosjga"
+    "meeshoLink": "https://www.meesho.com/elegant-teardrop-emerald-green-pendant-necklace-set-stylish-silver-finish-crystal-jewelry-for-women-girls-perfect-for-party-formal-wear/p/bosjga",
+    "tryOnImage": "/assets/tryon_necklace_299971351.png",
+    "tryOnImageLeft": "/assets/tryon_earring_left_299971351.png",
+    "tryOnImageRight": "/assets/tryon_earring_right_299971351.png"
   },
   {
     "id": 299928595,
@@ -161,6 +195,8 @@ export const products = [
     "description": "Fancy Earrings & Studs",
     "price": "₹135",
     "image": "https://images.meesho.com/images/catalogs/299928595/cover/1/2/2acd71923fcca67a087ada2d6ce44608984042177e820bbd0ca8d14b8c520e0f83a075db4f73d81a5a721b6a577956e99eb804f7c046e936b3532c205c9a297a_512.jpg",
-    "meeshoLink": "https://www.meesho.com/unique-oxidized-silver-dancer-motif-jhumkas-traditional-artistic-hanging-earrings-for-women-girls-stylish-lightweight-ethnic-jewelry/p/bori0z"
+    "meeshoLink": "https://www.meesho.com/unique-oxidized-silver-dancer-motif-jhumkas-traditional-artistic-hanging-earrings-for-women-girls-stylish-lightweight-ethnic-jewelry/p/bori0z",
+    "tryOnImageLeft": "/assets/tryon_earring_left_299928595.png",
+    "tryOnImageRight": "/assets/tryon_earring_right_299928595.png"
   }
 ];
